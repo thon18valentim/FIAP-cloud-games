1513d9e65b825e4589ee9c9876a717309bdf34f4
Luiz Fonseca <japalug2005@gmail.com>
2025-10-27 22:51:43 -0300

    gamecontroller

---
+++ src/App.FCG.WebApi/Models/Dtos/GameInsertDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.FCG.WebApi.Models.Dtos;
+
+public class GameInsertDto
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    [MaxLength (500, ErrorMessage = "Campo pode ter no máximop 500 caracteres.")]
+    public string Description { get; set; }
+    [Required]
+    [MaxLength (20, ErrorMessage = "Campo pode ter no máximo 20 caracteres.")]
+    public string PublisherName { get; set; }
+    [Required]
+    public DateTime ReleaseDate { get; set; }
+    [Required]
+    [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser maior que zero.")]
+    public decimal Price { get; set; }
+}
---
+++ src/FCG.Games/Data/Repository/GameRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FCG.Core.Data;
+using FCG.Games.Models;
+
+namespace FCG.Games.Data.Repository;
+
+public class GameRepository : GenericRepository<Game> , IGameRepository
+{
+    private readonly GameContext _context;
+    public GameRepository(GameContext context) : base(context)
+    {
+        _context = context; 
+    }
+
+    public IUnitOfWork UnitOfWork => _context;
+
+    public async Task<Game?> GetByName(string name)
+    {
+        return await _context.Games
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Name == name);
+    }
+     
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+
+    public async Task<bool> Commit()
+    {
+       return await _context.Commit();
+    }
+}
