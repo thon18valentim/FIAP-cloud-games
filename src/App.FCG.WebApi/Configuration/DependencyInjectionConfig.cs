db486e565c865bf42c3c7e1aed090974876853cc
Luiz Fonseca <japalug2005@gmail.com>
2025-10-28 16:17:10 -0300

    Adição Game Controller (v2)

--- src/App.FCG.WebApi/Configuration/DependencyInjectionConfig.cs
+++ src/App.FCG.WebApi/Configuration/DependencyInjectionConfig.cs
@@ -1,6 +1,8 @@
 ﻿using FCG.Clients.Data;
 using FCG.Clients.Data.Repository;
 using FCG.Clients.Services;
+using FCG.Games.Data;
+using FCG.Games.Data.Repository;
 using Microsoft.EntityFrameworkCore;
 
 namespace App.FCG.WebApi.Configuration
@@ -11,10 +13,13 @@ namespace App.FCG.WebApi.Configuration
         {
             builder.Services.AddDbContext<ClientsContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("Core")));
+            builder.Services.AddDbContext<GameContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Core")));
 
             // Registrar outros serviços, repositórios e unidades de trabalho aqui
             builder.Services.AddScoped<IClientRepository, ClientRepository>();
             builder.Services.AddScoped<IClientService, ClientService>();
+            builder.Services.AddScoped<IGameRepository, GameRepository>();
 
             return builder;
         }
---
+++ src/FCG.Games/Migrations/20251022001002_g-migration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCG.Games.Migrations
+{
+    /// <inheritdoc />
+    public partial class gmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Description = table.Column<string>(type: "varchar(100)", nullable: false),
+                    PublisherName = table.Column<string>(type: "varchar(100)", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
